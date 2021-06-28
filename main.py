5b6d2072a199a0497077719b8262484f5cf882f7
Sharaar <walidsharar@gmail.com>
2021-06-28 12:44:51 +0200

    project created

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
+user_input = input("Enter a word: ").upper()
 
+output_list=[phonetic_dict[letter] for letter in user_input]
+print(output_list)
