7b9d8884c95991c54886abd60ba3c243a640b7b9
Sharaar <walidsharar@gmail.com>
2021-06-28 20:59:25 +0200

    error handling

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-user_input = input("Enter a word: ").upper()
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        output_list=[phonetic_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry only letter in the alphabets please.")
+        generate_phonetic()
+    else:
+        print(output_list)
 
-output_list=[phonetic_dict[letter] for letter in user_input]
-print(output_list)
+generate_phonetic()
